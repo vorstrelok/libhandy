7e5f327203faab00599949c713a890a5003591a1
vorstrel <porochkin@gmail.com>
2017-10-16 22:20:22 +0300

    background substraction sorta works
    I think it's because if we learn too fast, center of the hand will be black,
    and if we learn too slow person's movement inside the frame will interfere too
    much.

--- libhandy/core.py
+++ libhandy/core.py
@@ -6,26 +6,34 @@ def main():
     camera_feed = cv2.VideoCapture(0)
 
     ret, frame = camera_feed.read()
+    COLORSPACE = cv2.COLOR_BGR2HSV
 
-    avg1 = np.float32(frame)
-    avg2 = np.float32(frame)
+    # avg = np.float32(frame)
+    # avg = cv2.cvtColor(np.float32(frame), COLORSPACE)
+    bgsubs = cv2.createBackgroundSubtractorMOG2(varThreshold=100, detectShadows=False)
+    avg = frame
     library_name = 'libhandy v0.1'
 
-    while(True):
+    while True:
         # Capture frame-by-frame
         ret, frame = camera_feed.read()
 
         # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.cvtColor(frame, COLORSPACE)
 
-        cv2.accumulateWeighted(frame, avg1, 0.1)
+        # cv2.accumulateWeighted(frame, avg, 0.4)
 
-        res1 = cv2.convertScaleAbs(avg1)
+        # result_frame = frame - cv2.convertScaleAbs(avg)
+        # result_frame = frame - avg
+        result_frame = bgsubs.apply(frame, learningRate=0.01)
+        # kernel = np.ones((3, 3), np.uint8)
+        # fg_mask = cv2.erode(result_frame, kernel, iterations=1)
+        # result_frame = cv2.bitwise_and(frame, result_frame, mask=fg_mask)
 
         # Display the resulting frame
         cv2.namedWindow(library_name, cv2.WINDOW_NORMAL)
         # cv2.imshow(library_name, gray)
-        cv2.imshow(library_name, res1)
+        cv2.imshow(library_name, result_frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
